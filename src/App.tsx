e902d1a57a6e89add324237e88f37b0bbf5068e3
nasimrifat101 <nasimrifat101@gmail.com>
2024-01-15 13:33:45 +0600

    starter

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+const App = () => {
+  const [time, setTime] = useState({
+    hour: 0,
+    minute: 0,
+    second: 0,
+  });
+
+  useEffect(() => {
+    const updateCurrentTime = () => {
+      const date = new Date();
+      setTime({
+        hour: date.getHours() % 12 || 12,
+        minute: date.getMinutes(),
+        second: date.getSeconds(),
+      });
+    };
+
+    const intervalId = setInterval(updateCurrentTime, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div className="digital-clock">
+      <div className="time">
+        {String(time.hour).padStart(2, "0")} :{" "}
+        {String(time.minute).padStart(2, "0")} :{" "}
+        {String(time.second).padStart(2, "0")}
+      </div>
+      <div className="date">
+        {new Date().toLocaleDateString("en-US", {
+          weekday: "long",
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default App;
