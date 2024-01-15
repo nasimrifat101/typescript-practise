bc3d34088642ffddcbaa53c74605c46cffcc1b39
nasimrifat101 <nasimrifat101@gmail.com>
2024-01-15 14:04:45 +0600

    routes setup

---
+++ src/components/Rifats/DigitalClock.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+
+const DigitalClock = () => {
+    const [time, setTime] = useState({
+        hour: 0,
+        minute: 0,
+        second: 0,
+      });
+    
+      useEffect(() => {
+        const updateCurrentTime = () => {
+          const date = new Date();
+          setTime({
+            hour: date.getHours() % 12 || 12,
+            minute: date.getMinutes(),
+            second: date.getSeconds(),
+          });
+        };
+    
+        const intervalId = setInterval(updateCurrentTime, 1000);
+    
+        return () => clearInterval(intervalId);
+      }, []);
+  return (
+    <div>
+      <div className="digital-clock">
+        <div className="time">
+          {String(time.hour).padStart(2, "0")} :{" "}
+          {String(time.minute).padStart(2, "0")} :{" "}
+          {String(time.second).padStart(2, "0")}
+        </div>
+        <div className="date">
+          {new Date().toLocaleDateString("en-US", {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DigitalClock;
